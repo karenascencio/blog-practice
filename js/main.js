f9721e2a0d7701c98d98068858a06382c5c86930
karenascencio <ascencio.karen@yahoo.com.mx>
2021-07-07 18:37:34 -0500

    added create post form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-let articleButtton = document.getElementById("article-button")
+let showMoreArticlesButton = document.getElementsByClassName("more-articles")
 const endpoint = "https://apikoder-b2ce0-default-rtdb.firebaseio.com/********/.json"
 let postObject = {}
-
-
