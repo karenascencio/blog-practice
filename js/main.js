59256a9e2b318db7401064fb8c7435d38432b919
chavus <nojiso@gmail.com>
2021-07-07 18:22:16 -0500

    Main.js layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Create/Post
+/*
+1.- Locate and Add listener to Post button
+2.- In the listener callback function:
+    a) Take input fields: Title, Content and ImageURL
+    b) Do a post to the endpoint to save the new object- Use an Async/xmlhttprequest function
+    c) Inside the async/xmlhttprequest function, render the post column in the aside
+    d) Each post should show: Title, image, content, 
+*/
+
+
+// 
