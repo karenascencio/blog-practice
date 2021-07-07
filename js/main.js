4d54838831b4c4cee30ba4a5b3e4586274edd3a3
chavus <nojiso@gmail.com>
2021-07-07 18:38:55 -0500

    Update to main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,24 @@
 2.- In the listener callback function:
     a) Take input fields: Title, Content and ImageURL
     b) Do a post to the endpoint to save the new object- Use an Async/xmlhttprequest function
-    c) Inside the async/xmlhttprequest function, render the post column in the aside
-    d) Each post should show: Title, image, content, 
+
+    c) Inside the async/xmlhttprequest function, render the post column in the aside. This should get the data from the endpoint
+
+    d) Each post should show: Title, image, content, and Show, Edit and Delete button. Add the id of the post in each button as a data- attribute
 */
+
+
+
+// Get/Post
+/*
+1.- Locate and Add listener to Post button
+2.- In the listener callback function:
+    a) Take input fields: Title, Content and ImageURL
+    b) Do a post to the endpoint to save the new object- Use an Async/xmlhttprequest function
+    c) Inside the async/xmlhttprequest function, render the post column in the aside
+    d) Each post should show: Title, image, content, and Show, Edit and Delete button. Add the id of the post in each button as a data- attribute
+*/
+
 
 
 // 
